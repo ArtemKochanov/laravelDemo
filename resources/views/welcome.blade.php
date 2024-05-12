cb483fcad37510b7e0f68b3e5c22301c891cc955
ArtemKochanov <artem06092001@yandex.ru>
2024-05-12 18:29:37 +0300

    123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,96 +4,43 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Book Shop</title>
 
-        <!-- Fonts -->
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <h1>Книжный магазин</h1>
+
+                <label>Найди нужную книгу: <input type='search' id='search_bar'>
+                    <input type="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Vector_search_icon.svg/1890px-Vector_search_icon.svg.png">
+                </label>
+                
+                <div class="product-wrap">
+                    <div class="product-image">
+                        <a href=""><img src="https://www.directmedia.ru/catalog/media/directmediashop/vs/rq/3f369614abf66bc6554b9d267576b017b3vztmyj2k.webp"></a>
+                        <div class="shadow"></div>
+                        <div class="actions">
+                        <div class="actions-btn">
+                            <div class="add-to-cart">
+                            <a class="add-to-cart-button" href="#" title="Добавить в корзину"></a>
+                            </div>
+                            <div class="add-to-links">
+                            <div class="add-to-wishlist">
+                                <a class="add-to-wishlist-button" href="#" title="Добавить в мои желания"></a>
+                            </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    <div class="product-list">
+                        <h3>Дон Кихот</h3>
+                        <p>Сервантес</p>
+                        <div class="price">&#8381; 1999</div>
+                    </div>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
